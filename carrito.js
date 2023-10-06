fdd23a950a7193109ecb52b1b8b0bdfa056ad7db
LauraMenendezTorres <laumenendez23@gmail.com>
2023-10-05 21:33:58 -0300

    proyecto final

--- carrito.js
+++ carrito.js
@@ -1,53 +1,102 @@
-const dbUnasVenta =  [
-    {id: 1, nombre: "Brillos para uñas", precio: 1000, cantidad: 1000},
-    {id: 2, nombre: "Esmalte fluor", precio: 500, cantidad: 1000},
-    {id: 3, nombre: "Kit de manicura 1", precio: 5200, cantidad: 1000},
-    {id: 4, nombre: "Esmalte", precio: 600, cantidad: 1000},
-    {id: 5, nombre: "Kit de manicura 2", precio: 6000, cantidad: 1000},
-    {id: 6, nombre: "Kit de manicura 3", precio: 6200, cantidad: 1000},
-    {id: 7, nombre: "Esmalte 2", precio: 700, cantidad: 1000},
+
+const dbUnasVenta = [
+    { id: 1, nombre: "Brillos para uñas", precio: 1000, cantidad: 1000 },
+    { id: 2, nombre: "Esmalte fluor", precio: 500, cantidad: 1000 },
+    { id: 3, nombre: "Kit de manicura 1", precio: 5200, cantidad: 1000 },
+    { id: 4, nombre: "Esmalte", precio: 600, cantidad: 1000 },
+    { id: 5, nombre: "Kit de manicura 2", precio: 6000, cantidad: 1000 },
+    { id: 6, nombre: "Kit de manicura 3", precio: 6200, cantidad: 1000 },
+    { id: 7, nombre: "Esmalte 2", precio: 700, cantidad: 1000 },
 ];
 
 const carrito = []
 
-const unasVenta = document.querySelector (".unas-venta__contenedor");
-function crearTemplate (){
-    unasVenta.innerHTML = "" ;
-    dbUnasVenta.forEach((producto) => {
-        const {id, nombre, precio} = producto
-        unasVenta.innerHTML += `
-        <section class="unas-venta__contenedor">
-        <h3 class="unas-venta__nombre">${nombre}</h3>
-        <span class="unas-venta__precio">Precio: $${precio}</span>
-        <button id= "${id}" class="btnAgregar"><i class="fa-solid fa-cart-shopping"></i></button>
-        </section>
-        `
-    })
+const btnCerrarCarrito = document.querySelector(".btnCerrarCarrito");
+const btnPagar = document.querySelector(".btnPagar");
+const totalElement = document.getElementById("total");
+
+function actualizarCarrito() {
+    const carritoItems = document.querySelector(".carrito-items");
+    carritoItems.innerHTML = "";
+    let total = 0;
+    carrito.forEach((producto) => {
+        const subtotal = producto.precio * producto.cantidad;
+        total += subtotal;
+        carritoItems.innerHTML += `
+        <div class="carrito-item">
+            <span class="carrito-item-nombre">${producto.nombre}</span>
+            <span class="carrito-item-precio">$${producto.precio}</span>
+            <span class="carrito-item-cantidad">${producto.cantidad}</span>
+            <span class="carrito-item-subtotal">$${subtotal.toFixed(2)}</span>
+            <button class="btn btn-danger btnEliminar" data-id="${producto.id}">Eliminar</button>
+        </div>
+        `;
+    });
+    totalElement.textContent = total.toFixed(2);
 }
 
-crearTemplate ()
+function agregarAlCarrito(id) {
+    const productoExistente = carrito.find((producto) => producto.id === id);
 
-document.addEventListener ("click", (e) => {
-    const btnAgregar = document.querySelectorAll (".btnAgregar")
-    btnAgregar.forEach((btn) => {
-        if (e.target == btn) {
-            const id = parseInt (e.target.id)
-            const producto = dbUnasVenta.map ((producto) => producto.id).indexOf (id)
-            agregarAlCarrito(producto)
-        }
-        })
-    })
-
-function agregarAlCarrito (indice){
-    const productoBuscar = carrito.find((productoCarrito) => productoCarrito.id === productoCarrito)
-    if (productoBuscar) {
-        productoBuscar.cantidad++
-    }else{
-        carrito.push ({
-            ...nombre,
-            cantidad: 1,
-        })
+    if (productoExistente) {
+        productoExistente.cantidad++;
+    } else {
+        const producto = dbUnasVenta.find((producto) => producto.id === id);
+        carrito.push({ ...producto, cantidad: 1 });
     }
-    console.log (carrito)
+
+    guardarCarrito();
+    actualizarCarrito();
+    mostrarCarrito();
 }
 
+function eliminarDelCarrito(id) {
+    const productoIndex = carrito.findIndex((producto) => producto.id === id);
+
+    if (productoIndex !== -1) {
+        carrito.splice(productoIndex, 1);
+        guardarCarrito();
+        actualizarCarrito();
+    }
+}
+
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function cargarCarrito() {
+    const carritoGuardado = localStorage.getItem("carrito");
+    if (carritoGuardado) {
+        carrito.push(...JSON.parse(carritoGuardado));
+        actualizarCarrito();
+    }
+}
+
+function mostrarCarrito() {
+    document.querySelector(".carrito-contenedor").classList.add("carrito-visible");
+}
+
+cargarCarrito();
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btnAgregar")) {
+        const id = parseInt(e.target.getAttribute("data-id"));
+        agregarAlCarrito(id);
+    }
+
+    if (e.target.classList.contains("btnEliminar")) {
+        const id = parseInt(e.target.getAttribute("data-id"));
+        eliminarDelCarrito(id);
+    }
+});
+
+btnCerrarCarrito.addEventListener("click", () => {
+    document.querySelector(".carrito-contenedor").classList.remove("carrito-visible");
+});
+
+btnPagar.addEventListener("click", () => {
+    alert("¡Gracias por tu compra! El total a pagar es $" + totalElement.textContent);
+    carrito.length = 0;
+    guardarCarrito();
+    actualizarCarrito();
+});
